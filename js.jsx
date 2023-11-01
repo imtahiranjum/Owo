aa00acb9394415c64e4cdc21a429e56afa2895cf
tahir_anjum <imtahiranjum@gmail.com>
2023-11-01 15:38:38 +0500

    Restored file

--- js.jsx
+++ js.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function js() {
   return (
     <div>
-        <div>js</div>
-        <div>js</div>
+      <div>js</div>
+      <div>js</div>
+      <div>js</div>
     </div>
-  )
+  );
 }
 
-export default js
+export default js;
